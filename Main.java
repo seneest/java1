d83239187cb553fbfc5db70a49074eb1eb3895ad
seneest <git@github.com:seneest/intervolga.git>
2021-02-01 19:05:51 +0300

    fifth lesson

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         empArr[0] = new employee("Nesterov Sergey", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
         empArr[1] = new employee("Kulchenko Ivan", "Programmer developer", "ivivan1@mailbox.com", "1892312312", 40000, 40);
         empArr[2] = new employee("Andreev Mikhail", "Administrator", "ivivan2@mailbox.com", "2892312312", 50000, 50);
-        empArr[3] = new employee("Sidorov  Nikita", "Secretary", "ivivan3@mailbox.com", "3892312312", 60000, 60);
+        empArr[3] = new employee("Sidorov Nikita", "Secretary", "ivivan3@mailbox.com", "3892312312", 60000, 60);
         empArr[4] = new employee("Abramovich Roman", "Manager", "ivivan4@mailbox.com", "4892312312", 70000, 70);
 
         for(employee item : empArr) {
